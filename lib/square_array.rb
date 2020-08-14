2789329c023a8a15f2041953a47d33a12f62d191
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-14 14:58:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   squared =  []
   count = 0 
-  while count < array.size do 
+  while count < array.length do 
     squared << (array[count] ** 2)
     count = count + 1 
   end 
